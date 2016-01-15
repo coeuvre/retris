76bda1f4ad1d93acfe2fbd1737085f5afacaf908
Coeuvre Wong <coeuvre@gmail.com>
2016-01-15 01:14:50 +0000

    Draw vertical and horizontal lines

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use sdl2::render::{Renderer, Texture};
 struct OffscreenBuffer<'a> {
     renderer: Renderer<'a>,
     buffer: Texture,
-    width: u32,
-    height: u32,
+    width: i32,
+    height: i32,
 }
 
 impl<'a> OffscreenBuffer<'a> {
@@ -19,13 +19,13 @@ impl<'a> OffscreenBuffer<'a> {
             buffer: renderer.create_texture_streaming(PixelFormatEnum::RGBA8888, (width, height))
                             .unwrap(),
             renderer: renderer,
-            width: width,
-            height: height,
+            width: width as i32,
+            height: height as i32,
         }
     }
 
     pub fn present(&mut self, window_width: u32, window_height: u32) {
-        self.renderer.clear();
+        // self.renderer.clear();
         self.renderer.copy(&self.buffer,
                            None,
                            Rect::new(0, 0, window_width, window_height).unwrap());
@@ -56,6 +56,68 @@ impl<'a> OffscreenBuffer<'a> {
             })
             .unwrap();
 
+    }
+
+    pub fn draw_hline(&mut self, mut y: i32, mut x_min: i32, mut x_max: i32, color: u32) {
+        y = self.clamp_y(y);
+        x_min = self.clamp_x(x_min);
+        x_max = self.clamp_x(x_max);
+
+        self.buffer
+            .with_lock(None, |buffer: &mut [u8], pitch: usize| {
+                unsafe {
+                    let mut row = buffer.as_mut_ptr();
+                    row = row.offset((y as usize * pitch) as isize);
+                    let mut pixel = row as *mut u32;
+                    pixel = pixel.offset(x_min as isize);
+                    for _ in 0..(x_max - x_min) {
+                        *pixel = color;
+                        pixel = pixel.offset(1);
+                    }
+                }
+            })
+            .unwrap();
+    }
+
+    pub fn draw_vline(&mut self, mut x: i32, mut y_min: i32, mut y_max: i32, color: u32) {
+        x = self.clamp_x(x);
+        y_min = self.clamp_y(y_min);
+        y_max = self.clamp_y(y_max);
+
+        self.buffer
+            .with_lock(None, |buffer: &mut [u8], pitch: usize| {
+                unsafe {
+                    let mut col = buffer.as_mut_ptr();
+                    col = (col as *mut u32).offset(x as isize) as *mut u8;
+                    col = col.offset((y_min as usize * pitch) as isize);
+                    for _ in y_min..y_max {
+                        let mut pixel = col as *mut u32;
+                        *pixel = color;
+                        col = col.offset(pitch as isize);
+                    }
+                }
+            })
+            .unwrap();
+    }
+
+    fn clamp_x(&self, x: i32) -> i32 {
+        if x < 0 {
+            0
+        } else if x >= self.width {
+            self.width - 1
+        } else {
+            x
+        }
+    }
+
+    fn clamp_y(&self, y: i32) -> i32 {
+        if y < 0 {
+            0
+        } else if y >= self.height {
+            self.height - 1
+        } else {
+            y
+        }
     }
 }
 
@@ -90,6 +152,8 @@ fn main() {
         }
 
         buffer.render_weird_graient(x_offset, y_offset);
+        buffer.draw_hline(10, 100, width as i32, 0x00000000);
+        buffer.draw_vline(100, 10, height as i32, 0x00000000);
         buffer.present(width, height);
 
         x_offset += 1;
